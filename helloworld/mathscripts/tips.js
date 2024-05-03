0059d494a84ce0266620894a709c5989256c773b
sreevidya <sreevidya-bathalapa@pluralsight.com>
2024-05-03 20:01:14 +0530

    added exercise 1

---
+++ helloworld/mathscripts/tips.js
@@ -0,0 +1,9 @@
+let foodCost =79.25;
+let tax=6.54;
+let tip=12.00;
+
+//calculations
+let totalDue=foodCost+tax+tip;
+console.log("Food cost is"+foodCost+"and tax is "+tax);
+console.log("tip is "+tip.toFixed(2));
+console.log("total due is"+totalDue);
