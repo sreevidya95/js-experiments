ea9ad3c609a7fff17a5aa5bd42bf61289180b076
sreevidya <sreevidya-bathalapa@pluralsight.com>
2024-05-08 15:16:19 +0530

    script for array filter and find concepts

---
+++ objectscripts/es6_course_search.js
@@ -0,0 +1,42 @@
+let courses = [
+    {
+        CourseId: "PROG100",
+        Title: "Introduction to HTML/CSS/Git",
+        Location: "Classroom 7",
+        StartDate: "09/08/24",
+        Fee: "100.00",
+    },
+    {
+        CourseId: "PROG200",
+        Title: "Introduction to JavaScript",
+        Location: "Classroom 9",
+        StartDate: "11/22/24",
+        Fee: "350.00",
+    },
+    {
+        CourseId: "PROG300",
+        Title: "Introduction to Java",
+        Location: "Classroom 1",
+        StartDate: "01/09/24",
+        Fee: "50.00",
+    },
+    {
+        CourseId: "PROG400",
+        Title: "Introduction to SQL and Databases",
+        Location: "Classroom 7",
+        StartDate: "03/16/24",
+        Fee: "50.00",
+    },
+    {
+        CourseId: "PROJ500",
+        Title: "Introduction to Angular",
+        Location: "Classroom 1",
+        StartDate: "04/25/24",
+        Fee: "50.00",
+    }
+];
+console.log(courses.find((course)=>course.CourseId=="PROG200").StartDate);
+console.log(courses.find((course)=>course.CourseId=="PROJ500").Title);
+courses.filter((course)=>course.Fee<=50).forEach((s)=>console.log(s.Title));
+courses.filter((course)=>course.Location=='Classroom 1').forEach((s)=>console.log(s.CourseId));
+
